e7a3285dca0cd837e839494b23046f2200ff131b
andreavgomez <you@example.com>
2020-04-24 19:18:43 -0300

    Trabajo Practico Adicion

---
+++ Adicion/adicion.cpp
@@ -0,0 +1,16 @@
+/* Adicion
+Andrea Gomez
+24/04/2020
+*/
+
+#include <iostream>
+
+int main() {
+    int a,b;
+    std::cout << "ingrese el primer valor para la suma \n";
+    std::cin >> a;
+    std::cout << "ingrese el segundo valor para la suma \n";
+    std::cin >> b;
+    std::cout << "El resultado de la suma es \n";
+    std::cout << a + b;
+}
